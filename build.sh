771f9325084f4e034e04800a5683556955f2cb57
winniehell <git@winniehell.de>
2025-06-10 22:34:09 +0200

    Setup wasm-pack project

--- build.sh
+++ build.sh
@@ -3,15 +3,6 @@
 set -o errexit
 set -o nounset
 
-cd "$(dirname "$0")/frontend/"
+cd "$(dirname "$0")/wasm/"
 
-# https://docs.rs/getrandom/0.3.3/getrandom/#webassembly-support
-export RUSTFLAGS='--cfg getrandom_backend="wasm_js"'
-
-#nix-shell \
-#  --run 'cargo build --target wasm32-unknown-unknown' \
-#  --packages llvmPackages_17.clang-unwrapped llvmPackages_17.bintools-unwrapped
-
-nix-shell \
-  --run 'dx build --trace --verbose' \
-  --packages llvmPackages_17.clang-unwrapped llvmPackages_17.bintools-unwrapped
+wasm-pack build
