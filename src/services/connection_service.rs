b9905c92e1ec159130711125ec13583ac6dc2bc8
winniehell <git@winniehell.de>
2025-09-22 16:17:43 +0200

    Make it a bit compatible with new peer messages (0.7.0)

--- src/services/connection_service.rs
+++ src/services/connection_service.rs
@@ -1,7 +1,8 @@
 use crate::services::automerge_service::{AutomergeCommand, FormattedAutomergeMessage};
 use crate::services::node_service::NODE_INFO;
-use anyhow::{anyhow, Error, Result};
+use anyhow::{anyhow, Context, Error, Result};
 use automerge::sync::Message as AutomergeSyncMessage;
+use derive_more::{Deref, Display};
 use dioxus::hooks::UnboundedReceiver;
 use dioxus::prelude::{
     spawn, use_coroutine_handle, Coroutine, GlobalSignal, ReadableOptionExt, Signal,
@@ -9,6 +10,9 @@ use dioxus::prelude::{
 use futures::StreamExt;
 use iroh::endpoint::{Connection, RecvStream, SendStream};
 use iroh::NodeId;
+use postcard::{from_bytes, to_allocvec};
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 use tokio::sync::broadcast;
 use tokio::sync::broadcast::{Receiver, Sender};
 
@@ -28,7 +32,51 @@ pub enum ConnectionCommand {
     },
 }
 
-pub async fn receive_message(receive: &mut RecvStream) -> Result<AutomergeSyncMessage> {
+pub type CursorId = String;
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Position {
+    pub line: usize,
+    pub character: usize,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Range {
+    pub start: Position,
+    pub end: Position,
+}
+
+/// Paths like these are relative to the shared directory.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Eq, Hash, Deref, Display)]
+#[display("'{}'", self.0.display())]
+#[must_use]
+pub struct RelativePath(PathBuf);
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Clone, Debug)]
+pub struct CursorState {
+    pub name: Option<String>,
+    pub file_path: RelativePath,
+    pub ranges: Vec<Range>,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Clone, Debug)]
+pub struct EphemeralMessage {
+    pub cursor_id: CursorId,
+    pub sequence_number: usize,
+    pub cursor_state: CursorState,
+}
+
+#[derive(Deserialize, Serialize)]
+/// The `PeerMessage` is used for peer to peer data exchange.
+pub enum PeerMessage {
+    /// The Sync message contains the changes to the CRDT
+    Sync(Vec<u8>),
+    /// The Ephemeral message currently is used for cursor messages, but can later be used for
+    /// other things that should not be persisted.
+    Ephemeral(EphemeralMessage),
+}
+
+async fn receive_peer_message(receive: &mut RecvStream) -> Result<PeerMessage> {
     let mut message_len_buf = [0; 4];
     receive.read_exact(&mut message_len_buf).await?;
     let message_len = u32::from_be_bytes(message_len_buf);
@@ -36,7 +84,17 @@ pub async fn receive_message(receive: &mut RecvStream) -> Result<AutomergeSyncMe
     let mut message_buf = vec![0; message_len as usize];
     receive.read_exact(&mut message_buf).await?;
 
-    Ok(AutomergeSyncMessage::decode(&message_buf)?)
+    from_bytes(&message_buf).context("Failed to convert bytes to PeerMessage")
+}
+
+async fn receive_message(receive: &mut RecvStream) -> Result<Option<AutomergeSyncMessage>> {
+    match receive_peer_message(receive).await? {
+        PeerMessage::Sync(message_buf) => Ok(Some(AutomergeSyncMessage::decode(&message_buf)?)),
+        PeerMessage::Ephemeral(_message_buf) => {
+            // TODO: implement the ephemerality
+            Ok(None)
+        }
+    }
 }
 
 fn start_receiving_messages(
@@ -45,14 +103,16 @@ fn start_receiving_messages(
     automerge_service: Coroutine<AutomergeCommand>,
 ) {
     spawn(async move {
-        while let Ok(message) = receive_message(&mut receive).await {
-            match FormattedAutomergeMessage::new("received", remote_node_id, &message) {
-                Ok(formatted_message) => {
-                    automerge_service.send(AutomergeCommand::ApplyMessage { message });
-                    AUTOMERGE_MESSAGES.write().push(formatted_message);
-                }
-                Err(error) => {
-                    CONNECTION_ERRORS.write().push(error);
+        while let Ok(maybe_message) = receive_message(&mut receive).await {
+            if let Some(message) = maybe_message {
+                match FormattedAutomergeMessage::new("received", remote_node_id, &message) {
+                    Ok(formatted_message) => {
+                        automerge_service.send(AutomergeCommand::ApplyMessage { message });
+                        AUTOMERGE_MESSAGES.write().push(formatted_message);
+                    }
+                    Err(error) => {
+                        CONNECTION_ERRORS.write().push(error);
+                    }
                 }
             }
         }
@@ -62,7 +122,8 @@ fn start_receiving_messages(
 }
 
 pub async fn send_message(send: &mut SendStream, message: AutomergeSyncMessage) -> Result<()> {
-    let message_buf = message.encode();
+    let peer_message = PeerMessage::Sync(message.encode());
+    let message_buf = to_allocvec(&peer_message)?;
     let message_len = u32::try_from(message_buf.len())?;
     send.write_all(&message_len.to_be_bytes()).await?;
 
