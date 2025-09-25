c7a48794928bdf12e1b4b4f196bfd2edd02c2dcc
winniehell <git@winniehell.de>
2025-09-25 17:51:44 +0200

    Load initial doc and switch to AutoCommit

--- src/services/automerge_service.rs
+++ src/services/automerge_service.rs
@@ -1,7 +1,7 @@
 use crate::services::connection_service::ConnectionCommand;
-use anyhow::{bail, Error, Result};
+use anyhow::{anyhow, bail, Error, Result};
 use automerge::sync::{Message as AutomergeSyncMessage, State as SyncState, State, SyncDoc};
-use automerge::{Automerge, ChangeHash, ObjId, ReadDoc};
+use automerge::{AutoCommit, ChangeHash, ObjId, ReadDoc};
 use chrono::{DateTime, Local};
 use dioxus::hooks::use_coroutine_handle;
 use dioxus::prelude::{Coroutine, GlobalSignal, Readable, Signal};
@@ -85,16 +85,16 @@ pub static FILES: GlobalSignal<Vec<String>> = Signal::global(Vec::new);
 pub static SELECTED_FILE: GlobalSignal<Option<AutomergeDocumentFile>> = Signal::global(|| None);
 
 async fn apply_message(
-    doc: &mut Automerge,
+    doc: &mut AutoCommit,
     state: &mut State,
     message: AutomergeSyncMessage,
 ) -> Result<Vec<ChangeHash>> {
     let mut new_doc = doc.fork();
-    new_doc.receive_sync_message(state, message)?;
+    new_doc.sync().receive_sync_message(state, message)?;
     Ok(doc.merge(&mut new_doc)?)
 }
 
-fn object_id_by_name(doc: &Automerge, parent: ObjId, name: &str) -> Result<ObjId> {
+fn object_id_by_name(doc: &AutoCommit, parent: ObjId, name: &str) -> Result<ObjId> {
     if let Some(object_id) = doc.get(parent, name)?.map(|entry| entry.1) {
         return Ok(object_id);
     }
@@ -102,20 +102,20 @@ fn object_id_by_name(doc: &Automerge, parent: ObjId, name: &str) -> Result<ObjId
     bail!("no object '{name}' found!")
 }
 
-fn files_object(doc: &Automerge) -> Result<ObjId> {
+fn files_object(doc: &AutoCommit) -> Result<ObjId> {
     object_id_by_name(doc, automerge::ROOT, "files")
 }
 
-fn files(doc: &Automerge) -> Result<Vec<String>> {
+fn files(doc: &AutoCommit) -> Result<Vec<String>> {
     Ok(doc.keys(files_object(doc)?).collect())
 }
 
-fn file_content(doc: &Automerge, file_name: &str) -> Result<String> {
+fn file_content(doc: &AutoCommit, file_name: &str) -> Result<String> {
     let object_id = object_id_by_name(doc, files_object(doc)?, file_name)?;
     Ok(doc.text(object_id)?)
 }
 
-fn select_file(doc: &Automerge, file_name: &str) -> Result<()> {
+fn select_file(doc: &AutoCommit, file_name: &str) -> Result<()> {
     *SELECTED_FILE.write() = Some(AutomergeDocumentFile {
         file_name: file_name.to_owned(),
         content: file_content(doc, file_name)?,
@@ -130,7 +130,7 @@ pub enum AutomergeCommand {
 }
 
 async fn handle_automerge_command(
-    doc: &mut Automerge,
+    doc: &mut AutoCommit,
     state: &mut State,
     command: AutomergeCommand,
     connection_service: Coroutine<ConnectionCommand>,
@@ -161,7 +161,7 @@ async fn handle_automerge_command(
             select_file(doc, file_name)?;
         }
         AutomergeCommand::StartSync => {
-            while let Some(message) = doc.generate_sync_message(state) {
+            while let Some(message) = doc.sync().generate_sync_message(state) {
                 let details = MessageDetails::from_message(&message)?;
                 AUTOMERGE_EVENTS
                     .write()
@@ -187,7 +187,22 @@ pub async fn start_automerge_service(mut commands_rx: UnboundedReceiver<Automerg
     let connection_service = use_coroutine_handle::<ConnectionCommand>();
 
     // TODO: load content from local storage?
-    let mut doc = Automerge::default();
+    // see https://github.com/ethersync/ethersync/blob/v0.7.0/daemon/src/document.rs#L37
+    let initial_doc = [
+        133, 111, 74, 131, 61, 157, 231, 85, 0, 118, 1, 16, 120, 107, 104, 47, 215, 9, 76, 32, 132,
+        136, 60, 124, 152, 120, 144, 182, 1, 143, 164, 31, 13, 102, 61, 139, 125, 246, 189, 135,
+        97, 16, 167, 63, 30, 215, 249, 60, 227, 113, 111, 61, 55, 138, 234, 94, 30, 142, 166, 78,
+        250, 6, 1, 2, 3, 2, 19, 2, 35, 2, 64, 2, 86, 2, 7, 21, 14, 33, 2, 35, 2, 52, 1, 66, 2, 86,
+        2, 128, 1, 2, 127, 0, 127, 1, 127, 2, 127, 0, 127, 0, 127, 7, 126, 5, 102, 105, 108, 101,
+        115, 6, 115, 116, 97, 116, 101, 115, 2, 0, 2, 1, 2, 2, 0, 2, 0, 2, 0, 0,
+    ];
+    let maybe_doc = AutoCommit::load(&initial_doc);
+    if let Err(error) = maybe_doc {
+        handle_error(anyhow!(error));
+        return;
+    }
+
+    let mut doc = maybe_doc.unwrap();
     let mut state = SyncState::default();
 
     while let Some(command) = commands_rx.next().await {
